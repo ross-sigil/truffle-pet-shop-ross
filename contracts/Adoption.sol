1544d30cd786827eb3d50ca4c486f3423eda0926
ross-sigil <ross@sigil.systems>
2021-06-23 17:36:00 -0500

    Minor changes to clean up code

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -6,7 +6,6 @@ contract Adoption {
 
 	function adopt(uint petID) public returns (uint){
         require(petID >= 0 && petID <= 15);
-
         adopters[petID] = msg.sender;
 
         return petID;
